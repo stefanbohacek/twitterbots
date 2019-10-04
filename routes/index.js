bfdb57a83dc292d5408bfd03c15ec71f7395634c
Glitch (twitterbots) <none>
2019-10-04 12:57:56 +0000

    Fixed data sync being initiated twice.

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,13 @@
 const express = require('express'),
       fs = require('fs'),
-      router = express.Router(),
-      dataSync = require( __dirname + '/../data-sync.js' );
+      router = express.Router();
 
-dataSync.sync( function( err, data ){
   router.get('/', function (req, res) { 
     res.render('../views/home.handlebars', {
       project_title: process.env.PROJECT_TITLE || 'New Project',
       project_description: process.env.PROJECT_DESCRIPTION || 'A simple project hosted on Glitch',
-      project_name: process.env.PROJECT_DOMAIN,
-      data: data,
-      dataStr: JSON.stringify( data )
+      project_name: process.env.PROJECT_DOMAIN
     });
   });
-} );
-
-
-
 
 module.exports = router;
